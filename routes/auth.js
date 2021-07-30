50392beef7b0cbc65e1ea0b0f9ce04d1ed4c7f44
Quentin Marignier <qmarignier@gmail.com>
2021-07-30 11:12:31 +0200

    pushy

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const router = express.Router();
-
+const User = require('../models/User')
 
 
 router.get("/signup", (req, res) => {
     res.render('signup')
+  });
+
+  router.post("/signup", async (req, res, next) => {
+    console.log('POST', req.body)
+    try{
+      const data = req.body
+      console.log(data)
+
+      const newUser = await User.create(data)
+
+      console.log('New user created: ', newUser)
+      res.redirect('/signin')
+    }
+    catch(err){
+      console.log('Err creating new user')
+
+      res.render('signup', {
+        msg : {status: "error", text: "Error creating new User"}
+      })
+    }
+    
   });
   
   router.get("/signin", (req, res) => {
