b2a91ebb15767b8661dad9d24766a9540a064073
Quentin Marignier <qmarignier@gmail.com>
2021-07-30 11:36:21 +0200

    log

--- routes/auth.js
+++ routes/auth.js
@@ -1,35 +1,76 @@
 const express = require("express");
 const router = express.Router();
 const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+const saltRounds = 10
 
 
 router.get("/signup", (req, res) => {
     res.render('signup')
   });
 
-  router.post("/signup", async (req, res, next) => {
-    console.log('POST', req.body)
-    try{
-      const data = req.body
-      console.log(data)
+router.post("/signup", async (req, res, next) => {
 
-      const newUser = await User.create(data)
+  try{
+    const data = req.body
 
-      console.log('New user created: ', newUser)
-      res.redirect('/signin')
-    }
-    catch(err){
-      console.log('Err creating new user')
+    if(!data.email || !data.password || !data.name || !data.lastname) throw new Error({status: "error", text: "All fields required"})
 
-      res.render('signup', {
-        msg : {status: "error", text: "Error creating new User"}
-      })
-    }
-    
-  });
+    const alreadyExist = await User.findOne({email: data.email})
+
+    if(alreadyExist) throw new Error({status: "error", text: "Email already exists"})
+
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedpwd = bcrypt.hashSync(password, salt);
+    data.password = hashedpwd
+
+    const newUser = await User.create(data)
+
+    res.redirect('/signin')
+    console.log('New user created: ', newUser)
+  }
+  catch(err){
+
+    res.render('signup', {
+      msg : {status: err.status, text: err.text}
+    })
+  }
+  console.log('Err creating new user')
+
+});
   
   router.get("/signin", (req, res) => {
     res.render("signin");
+  });
+
+  router.post("/signin", async (req, res, next) => {
+
+    try{
+      const {email, password} = req.body
+
+      const user = await User.findOne({email})
+
+      if(!user) throw new Error({status: "error", text: "Wrong credentials"})
+
+      const isValidPwd = bcrypt.compareSync(
+        password,
+        user.password
+      )
+
+      if(!isValidPwd) throw new Error({status: "error", text: "Wrong credentials"})
+
+      req.session.currentUser = user
+      res.redirect('/', {msg: "You are logged in !"})
+ 
+    }
+    catch(err){
+      console.log('Err connecting')
+
+      res.render('signin', {
+        msg : {status: err.status, text: err.text}
+      })
+    }
+    
   });
 
 module.exports = router;
